a9532d3512f1542b3a008d4c77665cb7aff22976
od1000665 <od1000665@otc.edu>
2025-09-24 21:21:31 -0500

    pseudo code
    pseudo code as comments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Leap Year Program
+    Created by: Owen Douglas
+    The program will ask the user for a year and decide if its a leap year through 
+    several operations
+*/
+
+//Asks the user for a 4 digit year
+
+//decide if the year/number is divisible by 4
+//If it is then move on
+
+//If not then its not a leap year
+
+//Then decide if the year/number is divisible by 100
+//If it is then move on
+
+//If not then its not a leap year
+
+//Finally decide if the year/number is divisible by 400
+//If it is then report the findings as a leap year
+
+//If not then its not a leap year
+
+//end
